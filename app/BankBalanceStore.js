140da8759c258be7fabfac739007c7c65d07f42f
Gerardo Arevalo <gerardo@atctech.co>
2016-07-24 00:18:00 -0400

    implementing flex/utils Store

--- app/BankBalanceStore.js
+++ app/BankBalanceStore.js
@@ -1,38 +1,30 @@
-import {EventEmitter} from 'fbemitter';
 import AppDispatcher from './AppDispatcher';
 import bankConstants from './constants';
+import {Store} from 'flux/utils';
 
-const CHANGE_EVENT = 'change';
-let __emitter = new EventEmitter();
 let balance = 0;
 
-let BankBalanceStore = {
-
+class BankBalanceStore extends Store {
     getState() {
         return balance;
-    },
+    } 
 
-    addListener: (callback) => {
-        return __emitter.addListener(CHANGE_EVENT, callback);
-    },
-};
-
-BankBalanceStore.dispatchToken = AppDispatcher.register((action) => {
-    switch (action.type) {
-        case bankConstants.CREATED_ACCOUNT:
-            balance = 0;
-            __emitter.emit(CHANGE_EVENT);
-            break;
-        case bankConstants.DEPOSITED_INTO_ACCOUNT:
-            balance = balance + action.amount;
-            __emitter.emit(CHANGE_EVENT);
-            break;
-        case bankConstants.WITHDREW_FROM_ACCOUNT:
-            balance = balance - action.amount;
-            __emitter.emit(CHANGE_EVENT);
-            break;
+    __onDispatch(action) {
+        switch (action.type) {
+            case bankConstants.CREATED_ACCOUNT:
+                balance = 0;
+                this.__emitChange();
+                break;
+            case bankConstants.DEPOSITED_INTO_ACCOUNT:
+                balance = balance + action.amount;
+                this.__emitChange();
+                break;
+            case bankConstants.WITHDREW_FROM_ACCOUNT:
+                balance = balance - action.amount;
+                this.__emitChange();
+                break;
+        }
     }
+}
 
-});
-
-export default BankBalanceStore;
+export default new BankBalanceStore(AppDispatcher);
